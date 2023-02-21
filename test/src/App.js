db5675f1f4a3ea579af6d65a4718a2535429386a
mo2men-5aled <momenk208@gmail.com>
2023-02-21 23:21:49 +0200

    update Readme file & app now taking bgColor from env variables

--- test/src/App.js
+++ test/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
 
 function App() {
+  console.log(process.env.REACT_APP_COLOR);
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{ backgroundColor: process.env.REACT_APP_COLOR }}
+    >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
